6c1912446ed7ceba0a16eb20833747b73b0a13ab
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-28 09:57:38 +0700

    konfigurasi komik js dan index.js

--- index.js
+++ index.js
@@ -10,5 +10,32 @@
 // git init
 // git branch -M main
 // git remote add origin https://github.com/syawalqi/248_ORM.git
-// membuat file /models/komik.js
+// 10 membuat file /models/komik.js
+// 11 buka mysql workbench buat database 
+// 12 create schema perpustakaan
+// 13 buka file .env isi konfigurasi database
+// password Gms041204#
+// 14 ke komik.js konfigurasi
+// 15 ke index.js konfigurasi
 
+
+const express = require("express");
+const app = express();
+require("./models");
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Server berjalan pada http://localhost:${port}`);
+})
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
---
+++ models/komik.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+
+    });
+
+    return Komik;
+};

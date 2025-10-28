c76b4dcff8dc9341ba4a83b91e5ddeda7eb46445
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-28 10:12:17 +0700

    membuat post dan get di index.js

--- index.js
+++ index.js
@@ -17,11 +17,16 @@
 // password Gms041204#
 // 14 ke komik.js konfigurasi
 // 15 ke index.js konfigurasi
+// buka package.json
+// 16 tambahkan script 
+// "start": "nodemon index.js"
+// 17 jalankan npm start
+// 18 lanjutkan membuat crud di index.js post dan get
 
 
 const express = require("express");
 const app = express();
-require("./models");
+const db = require("./models");
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,12 +35,33 @@ app.listen(port, () => {
     console.log(`Server berjalan pada http://localhost:${port}`);
 })
 
-db.sequelize.sync()
-    .then((result) => {
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        })
-    })
-    .catch((err) => {
-        console.log(err);
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
     });
+})
+
+.catch((err) => {
+    console.log(err);
+});
+
+app.post("/komik", async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    }
+    catch (err) {
+        res.send({ err });
+    }
+});
+
+app.get("/komik", async (req, res) => {
+    try {
+        const komiks = await db.Komik.findAll();
+        res.send(komiks);
+    }
+    catch (err) {
+        res.send({ err });
+    }
+});

7d1ea41ab45c1e067ede84eb4755d415e62c799a
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-28 09:37:33 +0700

    SETUP EXPRESS.JS

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1 git init -y
+// 2 buat file .gitignore dan .env
+// 3 gitignore /node_modules dan .env
+// 4 buat file .sequelizerc 
+// 5 npm install express mysql2 nodemon sequelize sequelize-cli dotenv
+// 6 npx sequelize init
+// 7 konifgurasi config/config.js
+// 8 buat file index.js diluar
+// 9 membuat github repo
+// git init
+// git branch -M main
+// git remote add origin https://github.com/syawalqi/248_ORM.git
+
